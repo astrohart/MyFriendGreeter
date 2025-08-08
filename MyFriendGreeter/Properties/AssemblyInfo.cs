2562745ca32ae318f91d4d842d309b312a285bbe
Brian Hart <brian.hart@xylogix.net>
2025-08-08 15:41:04 -0600

    Update AssemblyInfo.cs
    - Refreshed COM interop GUIDs in all `AssemblyInfo.cs` file(s).

--- MyFriendGreeter/Properties/AssemblyInfo.cs
+++ MyFriendGreeter/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("785086ea-b504-44b2-a97b-3429942b954b")]
+[assembly: Guid("06fd0d2c-e5aa-444a-bfb0-78dbad195d52")]
 
 // Version information for an assembly consists of the following four values:
 //
