32eea91ed91a07389fbcda84008e8c8c75b37d55
Brian Hart <brian.hart@xylogix.net>
2025-09-21 17:56:01 -0600

    Update EnglishGreetingStrategy.cs
    - Replacing all occurrences of `object sender` and `EventArgs e`, respectively, are prefaced with `[NotLogged]`.  This is being done so as to ensure that the value(s) of these parameter(s) do not get written to PostSharp log file(s).  Also updated all auto-property(ies) to make sure their getters and setters are all prefixed with the `[DebuggerStepThrough]` attribute.

--- MyFriendGreeter/EnglishGreetingStrategy.cs
+++ MyFriendGreeter/EnglishGreetingStrategy.cs
@@ -2,7 +2,7 @@
 {
     public class EnglishGreetingStrategy : IGreetingStrategy
     {
-        public Language Language { get; } = Language.English;
+        public Language Language { [DebuggerStepThrough] get; } = Language.English;
 
         public string Greet(string name)
             => $"Hello, world, {name}!";
